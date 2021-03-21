b49072b016633f076f81b4107da84fe157467240
Sanjar <sanjarfayzullaev99@gmail.com>
2021-03-21 16:11:05 +0500

    finished gulp

--- gulp/tasks/font.js
+++ gulp/tasks/font.js
@@ -1,5 +1,5 @@
 module.exports = ()=>
  $.gulp.task('font',()=>
   $.gulp.src($.path.src.font)
-  .pipe(ttf2woff2())
+  .pipe($.gp.ttf2woff())
    .pipe($.gulp.dest($.path.build.font)).on('end',$.bs.reload))
--- gulp/tasks/img.js
+++ gulp/tasks/img.js
@@ -1,5 +1,5 @@
-module.exports = ()=>
- $.gulp.task('img', ()=>
-  $.gulp.src($.path.src.img)
-  .pipe($.gulp.webp())
-   .pipe($.gulp.dest($.path.build.img)).on('end',$.bs.reload))
+module.exports = () =>
+  $.gulp.task('img', () =>
+    $.gulp.src($.path.src.img)
+    .pipe($.gp.webp())
+    .pipe($.gulp.dest($.path.build.img)).on('end', $.bs.reload))
